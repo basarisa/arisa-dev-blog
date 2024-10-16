1b6aaf4797791bd768eb0aad40022b81ca4c3c17
Arisa Teandum <basarisa.1302@gmail.com>
2024-10-16 16:27:30 +0700

    add vercel configuration for rewrite rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
